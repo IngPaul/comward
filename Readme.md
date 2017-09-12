e21362b5cee4e5fe519a8785acb690d12ec374ae
Paul Cuichan <ing.paul.cuixan@gmail.com>
2017-09-12 00:08:12 -0500

    actualizacion de la documentacion

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Instalacion
+1. Instalar Node js y NPM
+2. Ejecutar comando para instalar dependecias
+   ```
+    npm install
+   ```
+3. Ejecutar comando 
+   ```
+     npm start
+   ```
--- index.js
+++ index.js
@@ -4,27 +4,19 @@ http     = require("http"),
 server   = http.createServer(app),
 methodOverride = require("method-override"),
 bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
-
 var router = express.Router();
-
 router.get('/', function(req, res) {
  res.status(200).json({message:'hola', quien:"danny"});
 });
-
 router.get('/insert/:estado', function(req, res) {
   console.log("Informacion remitida")
   console.log(req.params.estado)
   res.send('Se ha recivido la peticion get');
 });
-
-
 app.use(router);
-
-
 app.listen(3000, function() {
    console.log("Node server running on http://localhost:3000");
 });
